01c912f8f74349fa18bc10a8505918ab31c6bd07
miang <@rorma95>
2024-05-31 12:53:56 -0600

    Primera entrega!

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-65646190-0c28-4286-b509-89fb57f39a16.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-e69dbb59-08d7-4bf7-b3b6-e5b9a947a280.containerhub.tripleten-services.com"
 DOC_PATH = "/docs/"
 LOG_MAIN_PATH = "/api/logs/main/"
 USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
